7f7e53c53484226a1b1a3712c96fbecc6ff61b05
user <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-23 11:31:57 -0500

    update for debian

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+fi
+echo $OS
+if [[ $OS =~ "Fedora" ]]; then
+  echo "fedora!"
+elif [[ $OS =~ "Debian" ]]; then
+  echo "debian detected"
+  sudo apt install python3-venv sshpass openssh-server -y
+  mkdir -p ~/source/virtual_envs
+  python3 -m venv ~/source/virtual_envs/ansible
+fi
 sudo systemctl start sshd
+source ~/source/virtual_envs/ansible/bin/activate
 pip install ansible
-ansible-playbook -i inventory tasks.yml --ask-become-pass -b -k
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory tasks.yml --ask-become-pass -b -k
 sudo systemctl stop sshd
+deactivate
 
