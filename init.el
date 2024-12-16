015268dd6fd08d3ce4c6201997c23a289329e0c8
drewskiwooskie <drewskiwooskie@gmail.com>
2024-12-16 08:10:14 -0500

    latest updates

--- init.el
+++ init.el
@@ -1,35 +1,66 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1) 
 (require 'package)
 (package-initialize)
 (setq package-enable-at-startup nil)
-(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("org"   . "https://orgmode.org/elpa/")
+                         ("gnu"   . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-
 
 
 ;; Bootstrap `use-package`
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(setq projectile-keymap-prefix (kbd "C-c p"))
-(global-set-key (kbd "C-x w") 'elfeed)
+
 (require 'use-package)
-(use-package markdown-mode :ensure t)
-(use-package markdown-preview-mode :ensure t)
-(use-package markdown-preview-eww :ensure t)
-(use-package doom-themes :ensure t)
-(use-package rust-mode :ensure t)
-(use-package rustic :ensure t)
-(use-package lsp-mode :ensure t)
+(use-package page-break-lines :ensure t)
+(use-package projectile :ensure t)
+(use-package all-the-icons :ensure t)
+(use-package nerd-icons :ensure t)
+(use-package helm :ensure t)
 (use-package company :ensure t)
-(use-package yasnippet :ensure t)
-(use-package flycheck :ensure t)
-(use-package go-mode :ensure t)
-(use-package elpy :ensure t)
-(use-package company-jedi :ensure t)
-(use-package poetry :ensure t)
+(use-package lsp-mode :ensure t)
+(use-package projectile :ensure t)
+(use-package neotree :ensure t)
+(use-package magit :ensure t)
+(use-package rg :ensure t)
+(use-package markdown-mode :ensure t)
+(use-package cyberpunk-theme :ensure t)
 
+;; use-package with package.el:
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(emms cyberpunk-theme rg magit neotree lsp-mode company markdown-mode projectile page-break-lines nerd-icons dashboard all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'cyberpunk t)
+(add-hook 'mpc-mode-hook
+ (lambda ()
+   (keymap-local-set "M-p"        'windmove-up)
+   (keymap-local-set "M-n"        'windmove-down)
+   (keymap-local-set "M-b"        'windmove-left)
+   (keymap-local-set "M-f"        'windmove-right)
+   (keymap-local-set "C-<return>" 'mpc-play-at-point)
+   (keymap-local-set "<SPC>"      'mpc-toggle-play)
+   (keymap-local-set "s"          'mpc-toggle-shuffle)
+   (keymap-local-set "n"          'next-line)
+   (keymap-local-set "p"          'previous-line)
+   (keymap-local-set "f"          'mpc-next)
+   (keymap-local-set "b"          'mpc-prev)))
+(emms-all)
+(setq emms-player-list '(emms-player-vlc)
+      emms-info-functions '(emms-info-native))
 
-(tool-bar-mode   -1)
-(scroll-bar-mode -1)
-(load-theme 'doom-one t)
--- install.sh
+++ install.sh
@@ -8,16 +8,27 @@ if [[ $OS =~ "Fedora" ]]; then
   echo "fedora detected"
   mkdir -p ~/source/virtual_envs
   python3 -m venv ~/source/virtual_envs/ansible
+  sudo dnf -y install sshpass python3-libdnf5
 elif [[ $OS =~ "Debian" ]]; then
   echo "debian detected"
-  sudo apt install python3-venv sshpass openssh-server -y
+  sudo apt install python3-venv sshpass openssh-server sshpass -y
+  mkdir -p ~/source/virtual_envs
+  python3 -m venv ~/source/virtual_envs/ansible
+elif [[ $OS =~ "EndeavourOS" ]]; then
+  echo "EndeavorOS detected"
+  sudo pacman -Sy sshpass
+  mkdir -p ~/source/virtual_envs
+  python3 -m venv ~/source/virtual_envs/ansible
+elif [[ $OS =~ "Arch Linux" ]]; then
+  echo "Arch Linux detected"
+  sudo pacman -Sy sshpass
   mkdir -p ~/source/virtual_envs
   python3 -m venv ~/source/virtual_envs/ansible
 fi
 sudo systemctl start sshd
 source ~/source/virtual_envs/ansible/bin/activate
 pip install ansible
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory tasks.yml --ask-become-pass -b -k
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory tasks.yml --ask-become-pass -b -k 
 sudo systemctl stop sshd
 deactivate
 
